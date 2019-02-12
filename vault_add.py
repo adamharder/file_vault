be01ac54d4707489d2a960b2340fa8f834da3213
Adam Harder <adam@adamharder.com>
2019-02-11 20:58:14 -0500

    fixed bug in vault path

--- vault_add.py
+++ vault_add.py
@@ -6,13 +6,25 @@ import os
 import sys
 import json
 
+
+
+def get_file_metadata(file_path:Path):
+    return dict(sha1=get_file_hash(file_path), file_path=[str(file_path.absolute())], extension=file_path.suffix.lower(), file_size=os.path.getsize(file_path))
+
 def add_file_to_vault(file_path:Path):
     assert(file_path.exists())
     if not does_file_exist(file_path):
         hash = get_file_hash(file_path)
-        metadata = dict(file_path=[str(file_path.absolute())], extension=file_path.suffix.lower())
+        metadata = get_file_metadata(file_path)
         print(json.dumps(metadata, indent=2))
+        if not (vault_folder()/hash[:2]).is_dir():
+            os.makedirs(vault_folder()/hash[:2])
+        (vault_folder()/hash[:2]/hash).write_bytes(file_path.read_bytes())
+        (vault_folder()/hash[:2]/f"{hash}_metadata.json").write_text(json.dumps(metadata, indent=2, sort_keys=True))
+
 
 if __name__=="__main__":
-    file_to_add = Path(sys.argv[1])
-    print(add_file_to_vault(file_to_add))
+    
+    for file_to_add in sys.argv[1:]:
+        file_to_add=Path(file_to_add)
+        print(add_file_to_vault(file_to_add))
--- vault_utils.py
+++ vault_utils.py
@@ -4,21 +4,26 @@ import os
 import sys
 
 def vault_folder()->Path:
-    return Path("./vault/archive")
+    return Path.home()/"vault"/"archive"
 
 if not vault_folder().is_dir():
     os.makedirs(vault_folder())
 
-def get_file_hash(file_path:Path):
-    file_hash = hashlib.sha1()
-    sha1 = hashlib.sha1()
+_file_hashes={}
 
-    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-    with open(file_path, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha1.update(data)
-    return sha1.hexdigest()
+def get_file_hash(file_path:Path):
+    path_str = str(file_path.absolute())
+    if not path_str in _file_hashes:
+        file_hash = hashlib.sha1()
+        sha1 = hashlib.sha1()
+
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+        with open(file_path, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+            _file_hashes[path_str]=sha1.hexdigest() 
+    return _file_hashes[path_str]
 
